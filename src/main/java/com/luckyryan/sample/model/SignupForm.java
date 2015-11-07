9d830c7583d34b63bb511499643e32455f8f3c30
your name <you@example.com>
2015-11-07 16:40:22 +0000

    first commit

---
+++ src/main/java/com/luckyryan/sample/model/SignupForm.java
@@ -0,0 +1,46 @@
+package com.luckyryan.sample.model;
+
+/**
+ * User: ryan
+ * Date: 2/7/13
+ */
+public class SignupForm {
+
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
